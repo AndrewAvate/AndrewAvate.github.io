a1124d502a9b0572ea91230493293718c8fcfd1d
Andrew Avate <3642542@gmail.com>
2015-12-05 22:46:21 +0500

    fix video

--- projects/Video/videobooth.js
+++ projects/Video/videobooth.js
@@ -89,7 +89,7 @@ function setVideo(e) {
 		pushUnpushButtons("video2", ["video1"]);
 	}
 
-	video.src = videos[id] +".mp4";//+ getFormatExtension(); fix to do later
+	video.src = videos[id] + getFormatExtension();
 	video.load();
 	video.play();
 
@@ -126,11 +126,11 @@ function isButtonPushed(id) {
 
 function getFormatExtension() {
 	var video = document.getElementById("video");
-
-	if(video.canPlayType("video/ogg") !== "") {
-		return ".ogv";
-	} else if(video.canPlayType("video/mp4") !== "") {
+	
+	if(video.canPlayType("video/mp4") !== "") {
 		return ".mp4";
+	} else if(video.canPlayType("video/ogg") !== "") {
+		return ".ogv";
 	} else if(video.canPlayType("video/webm") !== "") {
 		return ".webm";
 	}
